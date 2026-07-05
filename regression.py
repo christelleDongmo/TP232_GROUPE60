37ee7943e8965aeca50deaf18a7dcedc63bbc22e
paulette-aguimkeng <pauletteaguimkeng@facscience-uy1.cm>
2026-07-05 14:55:55 +0100

    ajout de la question2 : regression lineaire

---
+++ regression.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+df = pd.read_csv("data/eleves.csv")
+
+x = df["heures_etudes"]
+y = df["note"]
+
+plt.figure(figsize=(8,6))
+plt.scatter(x, y)
+
+a, b = np.polyfit(x, y, 1)
+x_line = np.linspace(x.min(), x.max(), 100)
+plt.plot(x_line, a*x_line+b, color="red", label=f"y = {a:.2f}x + {b:.2f}")
+
+
+plt.title("Relation entre les heures d'etude et la note")
+plt.xlabel("Heures d'etude")
+plt.ylabel("note")
+
+plt.grid(True)
+plt.savefig("figures/nuage_points.png")
+plt.show()
+
+correlation = x.corr(y)
+
+print("coefficient de correlation :", round(correlation, 3))
+
+a, b = np.polyfit(x, y, 1)
+print("pente :", a)
+print("ordonnee a l'origine :", b)
+
+r_squared = correlation**2
+print("R^2 :", round(r_squared, 3))
+
