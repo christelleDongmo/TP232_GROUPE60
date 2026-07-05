0916dad2d0d0ae6019f5074b18083ae6b47dd3e0
kongne valdex joyeux <valdexjoyeux@gmail.com>
2026-07-05 20:38:11 +0100

    utils

--- utils.py
+++ utils.py
@@ -1,4 +1,24 @@
-n = 200
+import unicodedata
+from random import random
+
+nom = "joyeux"
+n = 200 #taille du dataset
+
+
+def nom_f(nom):
+    nom_sans_accents = unicodedata.normalize("NFKD", nom)
+    nom_sans_accents = nom_sans_accents.encode("ascii", "ignore").decode("ascii")
+    return f"data/{nom_f(nom)}.csv"
+
+def nettoyer_nom(nom_sans_accents: str) -> str:    
+    nom_nettoye = "".join(c for c in nom_sans_accents if c.isalpha())
+    seed = sum([ord(c) for c in nom_nettoye])
+    return seed  
+
+
+    
+
+
 
 data_path = "data/eleves.csv"
 images_path = "images/"
